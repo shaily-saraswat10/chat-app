b4153051bed1e6154e8aa08f4dff838348e6fd48
shaily-saraswat10 <2024pgcsca023@nitjsr.ac.in>
2026-01-18 21:00:19 +0530

    final-one

--- server/server.js
+++ server/server.js
@@ -53,10 +53,13 @@ await connectDB()
 //start Server
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
+if(process.env.NODE_ENV !== "production")
+{
+    const PORT = process.env.PORT || 5000; 
+    server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
 
 //Export server for Vercel
 export default server;
