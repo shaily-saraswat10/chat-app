2ab7e09da6210da1afcede3d5631eb13cc19e760
shaily-saraswat10 <2024pgcsca023@nitjsr.ac.in>
2026-01-19 11:57:57 +0530

    last-commit

--- server/server.js
+++ server/server.js
@@ -51,10 +51,10 @@ app.use('/api/messages',messageRouter)
 await connectDB()
 
 //start Server
-    const PORT = process.env.PORT || 5000; 
-    server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+  if(process.env.NODE_ENV !== "production"){
+    const PORT = process.env.PORT || 5000
+    server.listen(PORT,()=>console.log(`Server is running on port ${PORT}`))
+}
 
 
 //Export server for Vercel
